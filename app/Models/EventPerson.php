96490468a958f8fd97c4783058acfc4dabe9a9aa
Neisser David Rey Gutierrez <neisserrey@upeu.edu.pe>
2022-12-11 21:22:02 -0500

    FIX: FALTABA EL FILLABLEEEEEEEEEEEEEEEEEEE

--- app/Models/EventPerson.php
+++ app/Models/EventPerson.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class EventPerson extends Pivot
 {
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['present'];
+
     protected $casts = [
         'present' => 'boolean'
     ];
